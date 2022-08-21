861d7ec34d2f8fbb8b4551ed8f11cfb3315bcdb1
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-08-21 20:57:29 +0200

    cuts under 30

--- script.py
+++ script.py
@@ -22,3 +22,7 @@ print('Total Revenue:', total_revenue)
 
 average_daily_revenue = total_revenue / 7
 print(average_daily_revenue)
+
+cuts_under_30 = [hairstyles[i]
+                 for i in range(len(new_prices) - 1) if new_prices[i] < 30]
+print(cuts_under_30)
