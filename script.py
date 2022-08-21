62d6e46b33194c3017280dc5a5a8bc5b23e968f5
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-08-21 20:56:21 +0200

    total_revenue

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
-hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+hairstyles = ["bouffant", "pixie", "dreadlocks",
+              "crew", "bowl", "bob", "mohawk", "flattop"]
 
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
@@ -6,9 +7,13 @@ last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
 total_price = 0
 for price in prices:
-  total_price += price
+    total_price += price
 
 average_price = total_price / len(prices)
 print('Average Haircut Price:', average_price)
 
 new_prices = [price - 5 for price in prices]
+
+total_revenue = 0
+for i in range(len(hairstyles)):
+    total_revenue += prices[i] * last_week[i]
